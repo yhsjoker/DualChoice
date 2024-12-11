33c5a03f19e4d2f87ed9e8b681ce88ff98e2a8fb
npcccccccccccccc <117692153+npcccccccccccccc@users.noreply.github.com>
2024-12-11 09:13:42 +0800

    计算初试总分mapper

--- src/main/java/cn/edu/bjfu/dualchoice/mapper/ExamMapper.java
+++ src/main/java/cn/edu/bjfu/dualchoice/mapper/ExamMapper.java
@@ -7,4 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 public interface ExamMapper {
     @Insert("INSERT INTO Exam(subject_id, student_id, type, score,time, location, comment) VALUES(#{subjectId}, #{studentId}, #{type}, #{score}, #{time}, #{location}, #{comment}) ")
     void insertExamInfo(int studentId, int subjectId, String type, float score, java.sql.Timestamp time, String location, String comment);
+
+    @Select("SELECT SUM(score) FROM Exam WHERE student_id = #{id}")
+    float getScore(int id);
 }
