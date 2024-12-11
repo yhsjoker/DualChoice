c4e1e14dee1bd21afb3e8f383333d377b1b23df8
npcccccccccccccc <117692153+npcccccccccccccc@users.noreply.github.com>
2024-12-11 18:30:43 +0800

    Update TeacherMapper.java

--- src/main/java/cn/edu/bjfu/dualchoice/mapper/TeacherMapper.java
+++ src/main/java/cn/edu/bjfu/dualchoice/mapper/TeacherMapper.java
@@ -4,6 +4,9 @@ import cn.edu.bjfu.dualchoice.pojo.Teacher;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
 
 @Mapper
 public interface TeacherMapper extends BaseMapper<Teacher> {
