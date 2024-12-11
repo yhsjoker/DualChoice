0b7d4c9d8b94424ab27f2f76ea9b4a665eab65c1
npcccccccccccccc <117692153+npcccccccccccccc@users.noreply.github.com>
2024-12-11 09:05:33 +0800

    完成5.5接口

--- src/main/java/cn/edu/bjfu/dualchoice/controller/GraduateManagementSecretaryController.java
+++ src/main/java/cn/edu/bjfu/dualchoice/controller/GraduateManagementSecretaryController.java
@@ -1,15 +1,116 @@
 package cn.edu.bjfu.dualchoice.controller;
 
-import cn.edu.bjfu.dualchoice.pojo.Result;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cn.edu.bjfu.dualchoice.pojo.*;
+import cn.edu.bjfu.dualchoice.service.*;
+import cn.edu.bjfu.dualchoice.utils.ThreadLocalUtil;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/graduateManagementSecretary")
 public class GraduateManagementSecretaryController {
+
+    @Autowired
+    GraduateManagementSecretaryService graduateManagementSecretaryService;
+
+    @Autowired
+    StudentService studentService;
+
+    @Autowired
+    TeacherService teacherService;
+
+    @Autowired
+    ApplicationService applicationService;
+
+    @Autowired
+    DisciplineService disciplineService;
+
+    @Autowired
+    ChoiceService choiceService;
+
+    @Autowired
+    StuBaseInfoService stuBaseInfoService;
+
+    @Autowired
+    DisciplineInfoService disciplineInfoService;
+
+    @Autowired
+    CollegeStudentInfoService collegeStudentInfoService;
+
     @GetMapping("/getCollege")
     public Result getCollege(){
+        Map<String, Object> map = ThreadLocalUtil.get();
+        int graduateManagementSecretaryId = (Integer) map.get("id");
+        String collegeName = graduateManagementSecretaryService.selectCollegeById(graduateManagementSecretaryId);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("collegeName", collegeName);
+
+        return Result.success(jsonObject);
+    }
+
+    @GetMapping("/students")
+    public Result getStudents(){
+        Map<String, Object> map = ThreadLocalUtil.get();
+        int graduateManagementSecretaryId = (Integer) map.get("id");
+        System.out.println(graduateManagementSecretaryId);
+        int collegeId = graduateManagementSecretaryService.selectCollegeIdById(graduateManagementSecretaryId);
+        JSONObject info = new JSONObject();
+        List<CollegeStudentInfo> studentList = collegeStudentInfoService.getCollegeStudentInfoById(collegeId);
+        List<JSONObject> stuInfoList = new ArrayList<>();
+        for (CollegeStudentInfo student : studentList) {
+            JSONObject stuInfo = new JSONObject();
+            stuInfo.put("id", student.getStudentId());
+            stuInfo.put("name", student.getStudentName());
+            stuInfo.put("examNumber", student.getStudentExamTicketNumber());
+            System.out.println(student.getStudentCategory());
+            stuInfo.put("category", student.getStudentCategory());
+
+            stuInfoList.add(stuInfo);
+        }
+
+        info.put("students", stuInfoList);
+
+        return Result.success(info);
+    }
+
+    @GetMapping("/teachers")
+    public Result getTeachers(){
+        Map<String, Object> map = ThreadLocalUtil.get();
+        int graduateManagementSecretaryId = (Integer) map.get("id");
+        int collegeId = graduateManagementSecretaryService.selectCollegeIdById(graduateManagementSecretaryId);
+        JSONObject info = new JSONObject();
+        List<Teacher> teacherList = teacherService.getTeacherListById(collegeId);
+        List<JSONObject> teaInfoList = new ArrayList<>();
+        for (Teacher teacher : teacherList) {
+            JSONObject teaInfo = new JSONObject();
+            teaInfo.put("id", teacher.getId());
+            teaInfo.put("name", teacher.getName());
+            teaInfo.put("title", teacher.getTitle());
+            teaInfo.put("phone", teacher.getPhone());
+            teaInfo.put("qualification", teacher.getQualification());
+
+            teaInfoList.add(teaInfo);
+        }
+
+        info.put("teachers", teaInfoList);
+
+        return Result.success(info);
+    }
+
+    @PutMapping("/teachers/setQualification")
+    public Result setQualification(@RequestBody Teacher teacher){
+        int teacherId = teacher.getId();
+        System.out.println(teacherId);
+        String qualification = teacher.getQualification();
+        System.out.println(qualification);
+        teacherService.setQualificationById(teacherId, qualification);
         return Result.success();
     }
+
 }
