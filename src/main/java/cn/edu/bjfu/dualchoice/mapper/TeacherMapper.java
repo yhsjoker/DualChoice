58e55c3188f4285139cbf026075cb687ee6ed213
npcccccccccccccc <117692153+npcccccccccccccc@users.noreply.github.com>
2024-12-11 09:16:59 +0800

    更新mapper

--- src/main/java/cn/edu/bjfu/dualchoice/mapper/TeacherMapper.java
+++ src/main/java/cn/edu/bjfu/dualchoice/mapper/TeacherMapper.java
@@ -10,5 +10,14 @@ public interface TeacherMapper extends BaseMapper<Teacher> {
     @Select("SELECT id FROM Teacher WHERE name = #{name}")
     int getTeacherIdByName(String name);
 
+    @Select("SELECT id, name, title, phone, qualification FROM Teacher")
+    List<Teacher> getTeacherList();
+
+    @Select("SELECT id, name, title, phone, qualification FROM Teacher WHERE college_id = #{id}")
+    List<Teacher> getTeacherListById(int id);
+
+    @Update("UPDATE Teacher SET qualification = #{qualification} WHERE id = #{id}")
+    void setQualificationById(int id, String qualification);
+
 
 }
