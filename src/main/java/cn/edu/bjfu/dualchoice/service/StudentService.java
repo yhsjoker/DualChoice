b9b0db1f16243eda9a1b98d500fb730b211c4f84
npcccccccccccccc <117692153+npcccccccccccccc@users.noreply.github.com>
2024-12-11 16:43:15 +0800

    Update StudentService.java

--- src/main/java/cn/edu/bjfu/dualchoice/service/StudentService.java
+++ src/main/java/cn/edu/bjfu/dualchoice/service/StudentService.java
@@ -14,6 +14,8 @@ public interface StudentService {
 
     List<String> selectDisciplineName(int id);
 
+    String selectGraduateType(int id);
+
     void updateResume(String url, int id);
 
     String selectResumeById(int id);
