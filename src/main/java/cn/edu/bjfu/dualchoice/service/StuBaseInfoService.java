df4226f7a79b2966ec03fc8a52bb8503e2b1720f
npcccccccccccccc <117692153+npcccccccccccccc@users.noreply.github.com>
2024-12-11 18:29:35 +0800

    Update StuBaseInfoService.java

--- src/main/java/cn/edu/bjfu/dualchoice/service/StuBaseInfoService.java
+++ src/main/java/cn/edu/bjfu/dualchoice/service/StuBaseInfoService.java
@@ -2,7 +2,13 @@ package cn.edu.bjfu.dualchoice.service;
 
 import cn.edu.bjfu.dualchoice.pojo.StuBaseInfo;
 
+import java.util.List;
+
 
 public interface StuBaseInfoService {
     StuBaseInfo getStuBaseInfoById(int id);
+
+    List<StuBaseInfo> getStuBaseInfoBydiscipline(String discipline);
+
+    List<StuBaseInfo> getStuBaseInfoByCollegeId(int id);
 }
