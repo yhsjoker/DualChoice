2ee46fd141757a77cab7ed7df794ca083e1a8713
npcccccccccccccc <117692153+npcccccccccccccc@users.noreply.github.com>
2024-12-11 18:29:14 +0800

    Update StuBaseInfoMapper.java

--- src/main/java/cn/edu/bjfu/dualchoice/mapper/StuBaseInfoMapper.java
+++ src/main/java/cn/edu/bjfu/dualchoice/mapper/StuBaseInfoMapper.java
@@ -4,8 +4,16 @@ import cn.edu.bjfu.dualchoice.pojo.StuBaseInfo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 @Mapper
 public interface StuBaseInfoMapper {
     @Select("SELECT * FROM StuBaseInfo WHERE studentId = #{id}")
     StuBaseInfo getStuBaseInfoById(int id);
+
+    @Select("SELECT studentId, name, examNumber, studentType FROM StuBaseInfo WHERE discipline = #{discipline}")
+    List<StuBaseInfo> getStuBaseInfoBydiscipline(String discipline);
+
+    @Select("SELECT studentId, name, examNumber, studentType FROM StuBaseInfo WHERE college_id = #{id}")
+    List<StuBaseInfo> getStuBaseInfoByCollegeId(int id);
 }
