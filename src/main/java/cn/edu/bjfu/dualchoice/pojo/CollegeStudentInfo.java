37f54318dde61717e04fe38416330e248c23ce71
npcccccccccccccc <117692153+npcccccccccccccc@users.noreply.github.com>
2024-12-11 09:21:46 +0800

    学院——学生视图

---
+++ src/main/java/cn/edu/bjfu/dualchoice/pojo/CollegeStudentInfo.java
@@ -0,0 +1,13 @@
+package cn.edu.bjfu.dualchoice.pojo;
+
+import lombok.Data;
+
+@Data
+public class CollegeStudentInfo {
+    int collegeId;
+    String collegeName;
+    int studentId;
+    String studentName;
+    String studentExamTicketNumber;
+    String studentCategory;
+}
